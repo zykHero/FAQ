9c0909639b6449e0c3cfcde5f07984c034a0b251
zykHero <1210424333@qq.com>
2019-10-31 21:57:31 +0800

    单选模板

--- src/app/manage-projects/my-project/createNewProject/radion-grop/radion-grop.component.ts
+++ src/app/manage-projects/my-project/createNewProject/radion-grop/radion-grop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-radion-grop',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./radion-grop.component.css']
 })
 export class RadionGropComponent implements OnInit {
-
-  constructor() { }
+  @Input() index:any = 1;
+  radioOptions:any = {};
+  options:any = [];
+  constructor(private ele:ElementRef) { }
 
   ngOnInit() {
+    this.createOptions(2);
+  }
+
+  createOptions(optionsNum){
+    let temp :any=[];
+    for(let i=0;i<optionsNum;i++){
+      temp.push({
+        label:`选项${i+1}`,
+        value:''
+      });
+    }
+    this.options = [...this.options,...temp];
+  }
+
+  removeOptions(index) {
+    this.options = this.options.filter((ele,i)=>{
+      return i !==index;
+    });
   }
 
 }
