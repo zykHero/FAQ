b12a8529b3c3292b16fe1a44b2aa8900732ad2c1
张远康-bigPC <1210424333@qq.com>
2019-11-01 00:05:47 +0800

    增加添加单项选则项

--- src/app/manage-projects/my-project/createNewProject/question-template/question-template.component.ts
+++ src/app/manage-projects/my-project/createNewProject/question-template/question-template.component.ts
@@ -6,27 +6,27 @@ import { RadionGropComponent } from '../radion-grop/radion-grop.component';
   styleUrls: ['./question-template.component.css']
 })
 export class QuestionTemplateComponent implements OnInit {
-  questionType:string = '';
-  questionIndex:number = 0;
+  questionType: string = '';
+  questionIndex: number = 0;
   questionGrout: any = [{
     id: 'radio',
     content: [{
-      title:"请选择一个选项",
-      radioGroup:[{
+      title: "请选择一个选项",
+      radioGroup: [{
         id: '',
         name: '选项1'
-      },{
+      }, {
         id: '',
         name: '选项2'
       }]
-    },{
+    }, {
 
     }]
-  },{
+  }, {
     id: 'multiple',
     content: [{
-      
-    },{
+
+    }, {
 
     }]
   }]
@@ -34,8 +34,8 @@ export class QuestionTemplateComponent implements OnInit {
 
   ngOnInit() {
   }
-  
-  showQusetionType(type,questionNBoxNum){
+
+  showQusetionType(type, questionNBoxNum) {
     this.questionType = type;
     this.questionIndex = questionNBoxNum
     console.log(this.questionIndex)
--- src/app/manage-projects/my-project/createNewProject/radion-grop/radion-grop.component.ts
+++ src/app/manage-projects/my-project/createNewProject/radion-grop/radion-grop.component.ts
@@ -1,35 +1,52 @@
-import { Component, OnInit,Input,ElementRef } from '@angular/core';
-
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-radion-grop',
   templateUrl: './radion-grop.component.html',
   styleUrls: ['./radion-grop.component.css']
 })
 export class RadionGropComponent implements OnInit {
-  @Input() index:any = 1;
-  radioOptions:any = {};
-  options:any = [];
-  constructor(private ele:ElementRef) { }
+  @Input() index: any = 1;
+  radioOptions: any = {};
+  options: any = [];
+  focusTemplate: boolean = true;
+  defaultOptionsNum: number = 2;
+  buttonsString: any = {
+    addRadio: this.translate.instant('project.addRadio'),
+    addRadioMore: this.translate.instant('project.addRadioMore')
+  };
+  constructor(private ele: ElementRef, public translate: TranslateService) { }
 
   ngOnInit() {
-    this.createOptions(2);
+    this.createDefaultOptions(2);
   }
 
-  createOptions(optionsNum){
-    let temp :any=[];
-    for(let i=0;i<optionsNum;i++){
-      temp.push({
-        label:`选项${i+1}`,
-        value:''
-      });
-    }
-    this.options = [...this.options,...temp];
+  addOptions() {
+    let tempArr = [{
+      label: `选项${this.options.length + 1}`,
+      value: ''
+    }];
+    this.options = [...this.options, ...tempArr];
   }
 
   removeOptions(index) {
-    this.options = this.options.filter((ele,i)=>{
-      return i !==index;
+    if (this.options.length === 2) {
+      alert("单项选择至少需要2项目");
+      return;
+    }
+    this.options = this.options.filter((ele, i) => {
+      return i !== index;
     });
   }
 
+  private createDefaultOptions(optionsNum) {
+    let temp: any = [];
+    for (let i = 0; i < optionsNum; i++) {
+      temp.push({
+        label: `选项${i + 1}`,
+        value: ''
+      });
+    }
+    this.options = [...this.options, ...temp];
+  }
 }
